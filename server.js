bc32828916999cb46eb67cd61ad85a6e4bdb62a1
Michał Orzechowski <m.orzechowski@adform.com>
2022-01-27 23:45:56 +0100

    ip

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const
-    {Server} = require("socket.io"),
-    server = new Server(80);
+import { Server } from "socket.io"
+
+const server = new Server(80);
 
 let
     sequenceNumberByClient = new Map(),
