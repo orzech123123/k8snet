b387f733a022c13ae4ee1f90ae6a9fb52cc60b98
orzech <orzech@DESKTOP-BE8VQUO>
2022-01-29 17:31:47 +0100

    client server changed to projacek

--- api/socket-server/client.js
+++ api/socket-server/client.js
@@ -6,7 +6,7 @@
 import { io } from "socket.io-client";
 import got from 'got';
 
-const ioClient = io.connect("http://localhost:80");
+const ioClient = io.connect("http://77.55.212.76/:80");
 
 ioClient.on("seq-num", (msg) => console.info(msg));
 
