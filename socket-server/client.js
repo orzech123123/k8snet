1827385b637cd031eac9eb191142040dba9881aa
orzech <orzech@DESKTOP-BE8VQUO>
2022-01-29 15:46:01 +0100

    test

---
+++ socket-server/client.js
@@ -0,0 +1,28 @@
+// const
+//     io = require("socket.io-client"),
+//     { exec } = require("child_process"),
+//     ioClient = io.connect("http://77.55.212.76:80");
+
+import { io } from "socket.io-client";
+import got from 'got';
+
+const ioClient = io.connect("http://77.55.212.76:80");
+
+ioClient.on("seq-num", (msg) => console.info(msg));
+
+ioClient.on("request-ip", async () => {
+    await got.get('http://api.ipify.org').text().then((ip) => ioClient.emit("receive-ip", ip));
+    
+    // exec("curl api.ipify.org", (error, stdout, stderr) => {
+    //     if (error) {
+    //         console.log(`error: ${error.message}`);
+    //         return;
+    //     }
+    //     if (stderr) {
+    //         console.log(`stderr: ${stderr}`);
+    //         return;
+    //     }
+        
+    //     ioClient.emit("receive-ip", stdout)
+    // });
+});
---
+++ socket-server/server.js
@@ -0,0 +1,38 @@
+import { Server } from "socket.io"
+
+const server = new Server(80);
+
+let
+    sequenceNumberByClient = new Map(),
+    ipByClient = new Map();
+
+// event fired every time a new client connects:
+server.on("connection", (socket) => {
+    console.info(`Client connected [id=${socket.id}]`);
+    // initialize this client's sequence number
+    sequenceNumberByClient.set(socket, 1);
+    ipByClient.set(socket, null);
+
+    // when socket disconnects, remove it from the list:
+    socket.on("disconnect", () => {
+        sequenceNumberByClient.delete(socket);
+        console.info(`Client gone [id=${socket.id}]`);
+    });
+    
+    socket.on("receive-ip", (ip) => {
+        ipByClient.set(socket, ip);
+    });
+});
+
+// sends each client its current sequence number
+setInterval(() => {
+    for (const [client, sequenceNumber] of sequenceNumberByClient.entries()) {
+        client.emit("seq-num", sequenceNumber);
+        sequenceNumberByClient.set(client, sequenceNumber + 1);
+    }
+
+    for (const [client, ip] of ipByClient.entries()) {
+        client.emit("request-ip");
+        console.log(` ${client} - ip: ${ip}`)
+    }
+}, 1000);
