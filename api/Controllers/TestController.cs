3d7f62ca3981c8f3ffb8afdcc428121a400678cc
orzech <orzech@DESKTOP-BE8VQUO>
2022-02-01 01:15:21 +0100

    selenium final test connection

--- api/Controllers/TestController.cs
+++ api/Controllers/TestController.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Remote;
 using System;
 using System.Drawing;
 using System.Threading;
@@ -54,7 +55,7 @@ namespace react_app.Controllers
             chromeOptions.AddArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36");
             Console.WriteLine("headless && user-agent set");
 
-            var m_driver = new ChromeDriver(@"/chromedriver", chromeOptions);
+            var m_driver = new RemoteWebDriver(new Uri("http://selenium:4444/wd/hub"), chromeOptions);
             m_driver.Url = "https://kubernetes.io/pl/docs/tutorials/kubernetes-basics/create-cluster/cluster-interactive/";
             m_driver.Manage().Window.Size = new Size(1024, 768);
             //IWebElement hideIntroHide = m_driver.FindElement(By.Id("hide-intro"));
